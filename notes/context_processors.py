0c6b4a771b133df366a2841a67b06c55492204fb
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-03-11 14:12:41 +0300

    Remove unused context processor

--- notes/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import *
-
-def data(request):
-    return {
-        'notes': Note.objects.all(),
-        'todolists': TodoList.objects.all(),
-        'tags': Tag.objects.all(),
-    }
